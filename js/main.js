30244e304b0be996610fe087aee9500b6c6404d1
Xalava <xavier.lavayssiere@gmail.com>
2015-02-13 03:12:58 +0100

    init

---
+++ js/main.js
@@ -0,0 +1,19 @@
+;(function(){
+
+			// Menu settings
+			$('#menuToggle, .menu-close').on('click', function(){
+				$('#menuToggle').toggleClass('active');
+				$('body').toggleClass('body-push-toleft');
+				$('#theMenu').toggleClass('menu-open');
+			});
+
+
+})(jQuery)
+
+
+
+$('#nav').affix({
+      offset: {
+        top: $('header').height()
+      }
+}); 
